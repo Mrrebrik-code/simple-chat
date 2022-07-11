3971533a675a397d802458cec0d57552ab27d23c
Mrrebrik <itibsoft@gmail.com>
2022-07-11 22:53:45 +0300

    - create demo scene UI

---
+++ client/Assets/Scripts/Core/ChatManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatManager : MonoBehaviour
+{
+	public void SendMessage()
+	{
+
+	}
+
+	public void JoinChat()
+	{
+
+	}
+
+	public void LeaveChat()
+	{
+
+	}
+
+
+}
